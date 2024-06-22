f3382c1bb6ae2a572d015892e15c4041c7596a85
Olalakan Otele <lekanbiodun@gmail.com>
2024-06-22 21:46:51 +0100

    First commit

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProductDisplay.css'
+
+const ProductDisplay = () => {
+  return (
+    <div className='Productdisplay'>ProductDisplay</div>
+  )
+}
+
+export default ProductDisplay

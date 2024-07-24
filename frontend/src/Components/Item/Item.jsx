a286d4578f924e1bd9d18bb4e00fe04d47ded588
Olalakan Otele <lekanbiodun@gmail.com>
2024-07-24 10:44:16 +0100

    Updating the components

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -6,16 +6,12 @@ const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-      <img src={props.image} alt="" />
+        <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" />
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">
-            N{props.new_price}
-        </div>
-        <div className="item-price-old">
-            N{props.old_price}
-        </div>
+        <div className="item-price-new">N{props.new_price}</div>
+        <div className="item-price-old">N{props.old_price}</div>
       </div>
     </div>
   );

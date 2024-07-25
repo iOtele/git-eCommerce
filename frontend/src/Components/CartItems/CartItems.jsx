cdf0b1ebdd0d2a3f338178756c82afbcc90d501e
Olalakan Otele <lekanbiodun@gmail.com>
2024-07-25 23:10:51 +0100

    Updated CartItems

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,8 @@ import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { getTotalCartAmount, all_product, cartItems, removeFromCart } =
+    useContext(ShopContext);
   return (
     <div className="cartitems">
       <div className="cartitems-format-main">
@@ -45,11 +46,11 @@ const CartItems = () => {
       })}
       <div className="cartitems-down">
         <div className="cartitems-total">
-          <h1>car Totals</h1>
+          <h1>Cart Totals</h1>
           <div>
             <div className="cartitems-total-item">
               <p>Subtotal</p>
-              <p>${0}</p>
+              <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cartitems-total-item">
@@ -57,9 +58,9 @@ const CartItems = () => {
               <p> Free</p>
             </div>
             <hr />
-            <div className="caritems-total-item">
+            <div className="cartitems-total-item">
               <h3>Total</h3>
-              <h3>$(0)</h3>
+              <h3>${getTotalCartAmount()}</h3>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import React,{useState} from "react";
+import React, { useContext, useState } from "react";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState('shop')
+  const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -14,15 +16,55 @@ const Navbar = () => {
         <p>BUYiT</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu('shop')}}><Link style={{textDecoration:"none"}} to="/">Shop</Link>{menu==='shop'?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu('mens')}}><Link style={{textDecoration:"none"}} to="/mens">Men</Link>{menu==='mens'?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu('women')}}><Link style={{textDecoration:"none"}} to="/women">Women</Link>{menu==='women'?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu('kids')}}><Link style={{textDecoration:"none"}} to="/kids">Kids</Link>{menu==='kids'?<hr/>:<></>}</li>
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/women">
+            Women
+          </Link>
+          {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
-      <Link to="/login"><button>Login</button></Link>
-      <Link to="/cart"><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
